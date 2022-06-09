ddc67c6781ed9cfffa73b015e7928d96853cb53a
brycecoder37 <73134123+brycecoder37@users.noreply.github.com>
2022-06-08 21:28:49 -0600

    improving side panel

--- src/components/nodeDescriptionPanel/NodeDescriptionPanelHelpers.js
+++ src/components/nodeDescriptionPanel/NodeDescriptionPanelHelpers.js
@@ -5,13 +5,34 @@ export function determineNodeInfo(node) {
     Insert conditional logic that shows certain information based on what type of node it is.
      */
 
-    if (node.name.includes("Measure")) console.log("is measure")
+    const node_type = determineNodeType(node);
 
     return (
         <>
-        <div className="node-name">{node.name}</div>
-        <div>Value: {node.value}</div>
-        <div>{node.description}</div>
+            <div className="node-name">{node.name}</div>
+            <div><b>Node Type: </b>{node_type}</div>
+            <div><b>Value: </b>{node.value.toFixed(5)}</div>
+            {node.description !== "" ? <div><b>Description: </b>{node.description}</div> : null}
+            {node_type === "Measure" ? <div><b>Thresholds: </b>[{getThresholds(node)}]</div> : null}
+            {node_type === "Diagnostic" ? <div><b>Tool:</b>{node.toolName}</div> : null}
         </>
     )
+}
+
+function determineNodeType(node) {
+    if (node.name.includes("Measure")) return "Measure";
+    else if (node.name.includes("Diagnostic")) return "Diagnostic";
+    else if (node.name.includes("Category")) return "Product Factor"
+    else if (node.name === "Binary Security Quality") return "TQI";
+    else return "Quality Aspect";
+}
+
+function getThresholds(node) {
+    const thresholds_array = node.thresholds;
+    let thresholds = "";
+    for (let i = 0; i < thresholds_array.length; i++) {
+        thresholds += (thresholds_array[i] === 0 ? "0" : thresholds_array[i].toFixed(4)) + ",";
+    }
+    thresholds = thresholds.substring(0,thresholds.length-1);
+    return thresholds;
 }
