0624f836f9df51f18868f5fd8196e367231f1971
brycecoder37 <73134123+brycecoder37@users.noreply.github.com>
2022-07-16 15:48:28 -0600

    added some checkers when uploading a file

--- src/components/fileHandling/UploadFile.js
+++ src/components/fileHandling/UploadFile.js
@@ -1,28 +1,55 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import TreeDisplay from "../treeDisplay/TreeDisplay";
 import "./UploadFile.css"
 
 export function UploadFile() {
     const [file, setFile] = useState("");
-    const [fileName, setFileName] = useState(null);
+    const [fileName, setFileName] = useState(null); // can find use later
     const [fileJSON, setFileJSON] = useState(null);
+    const [fileWorks,setFileWorks] = useState(false);
 
     const handleChange = (e) => {
         const fileReader = new FileReader();
         fileReader.readAsText(e.target.files[0], "UTF-8");
         setFileName(e.target.files[0].name);
+        const isJson = e.target.files[0].name.split(".")[1] === "json"
         // Set file and fileJSON according to file chosen.
         fileReader.onload = e => {
             setFile(e.target.result);
-            setFileJSON(JSON.parse(e.target.result))
+            if (isJson) setFileJSON(JSON.parse(e.target.result))
         };
     };
 
+    useEffect(() => {
+        if (fileJSON) setFileWorks(fileCanBeProcessed(fileJSON));
+    },[fileJSON])
+
     return (
         <div className="unselectableText">
-            {file ? <h4 id={"filename"}>{fileJSON.name}</h4> : <h4 align={"center"} unselectable={"on"}>Upload PIQUE json file to visualize the results.</h4>}
-            {file ? null : <input type="file" id={"fileChooser"} onChange={handleChange} />}
-            {file ? <TreeDisplay fileData={fileJSON}/> : null}
+            {file && fileWorks ? <h4 id={"filename"}>{fileJSON.name}</h4> : <h4 align={"center"} unselectable={"on"}>Upload PIQUE json file to visualize the results.</h4>}
+            {file && fileWorks ? null : <input type="file" id={"fileChooser"} onChange={handleChange} />}
+            {file && !fileWorks ? <div align={"center"} style={{"color" : "red"}}>Please upload PIQUE json file</div> : null}
+            {file && fileWorks ? <TreeDisplay fileData={fileJSON}/> : null}
         </div>
     );
+}
+
+/**
+ * Checks if the submitted json file has the necessary properties, because then
+ * it should be able to be processed correctly.
+ */
+function fileCanBeProcessed(jsonfile) {
+    const property_checkers = ["name","additionalData","global_config","factors","measures","diagnostics"];
+    const factor_checkers = ["product_factors","quality_aspects","tqi"];
+    for (let property in property_checkers) {
+        if (!jsonfile.hasOwnProperty(property_checkers[property])) {
+            return false;
+        }
+    }
+    for (let factor in factor_checkers) {
+        if (!jsonfile.factors.hasOwnProperty(factor_checkers[factor])) {
+            return false;
+        }
+    }
+    return true;
 }
