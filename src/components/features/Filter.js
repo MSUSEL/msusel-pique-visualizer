a8610281a23b6bc769fea2909baf023b625a04b1
ziyi zhang <ziyi.zhang2@wsu.edu>
2023-05-24 14:15:45 -0700

    add the feature filtering, now there are two buttons: quick filter to hide all 0 nodes, and filter (min, max) to let user type in a range, nodes whose values are out of range are filtered out

--- src/components/features/Filter.js
+++ src/components/features/Filter.js
@@ -4,12 +4,131 @@ import "./Features.css"
  */
 
 export function filterZero(fileData) {
-    const {filteredObj} = Object.values(fileData).filter((node) => node.value !== 0);       
-    return (filteredObj);
+    if (!fileData) {
+        return fileData;  // If fileData or factors is not defined, return original data
+    }
+
+    let filteredFileData = JSON.parse(JSON.stringify(fileData));
+
+    let dataArray_product_factors = filteredFileData.factors.product_factors ? Object.values(filteredFileData.factors.product_factors) : [];
+    console.log(dataArray_product_factors.length);
+    let dataArray_quality_aspects = filteredFileData.factors.quality_aspects ? Object.values(filteredFileData.factors.quality_aspects) : [];
+    let dataArray_measures = filteredFileData.measures ? Object.values(filteredFileData.measures) : [];
+    let dataArray_diagnostics = filteredFileData.diagnostics ? Object.values(filteredFileData.diagnostics) : [];
+
+    if (filteredFileData.factors && filteredFileData.factors.product_factors) {
+        let productFactors = filteredFileData.factors.product_factors;
+
+        Object.keys(productFactors).forEach(key => {
+            if (productFactors[key].value === 0) {
+                delete productFactors[key];
+            }
+        });
+    }
+
+    if (filteredFileData.factors && filteredFileData.factors.quality_aspects) {
+        let qualityAspects = filteredFileData.factors.quality_aspects;
+
+        Object.keys(qualityAspects).forEach(key => {
+            if (qualityAspects[key].value === 0) {
+                delete qualityAspects[key];
+            }
+        });
+    }
+
+    if (filteredFileData.measures) {
+        let measures = filteredFileData.measures;
+
+        Object.keys(measures).forEach(key => {
+            if (measures[key].value === 0) {
+                delete measures[key];
+            }
+        });
+    }
+
+    if (filteredFileData.diagnostics) {
+        let diagnostics = filteredFileData.diagnostics;
+
+        Object.keys(diagnostics).forEach(key => {
+            if (diagnostics[key].value === 0) {
+                delete diagnostics[key];
+            }
+        });
+    }
+
+    return (filteredFileData);
 }
 
+
+/**
+ * Return the filtered json object based on a range
+ * @param {Object} fileData - The JSON object to be filtered
+ * @param {number} min - The minimum value of the range (inclusive)
+ * @param {number} max - The maximum value of the range (inclusive)
+ * @returns {Object} - The filtered JSON object
+ * Todo: 
+ * @param {string} key - The key indicating the property to filter on 
+ */
+
+
+
+
 export function filterRange(fileData, min, max) {
-    let {filteredObj} = fileData.filter((node) => node.value >= min);     
-    filteredObj = filteredObj.filter((node) => node.value <= max);   
-    return (filteredObj);
+    if (!fileData) {
+        return fileData; // If fileData is not defined, return original data
+    }
+
+    let filteredFileData = JSON.parse(JSON.stringify(fileData));
+
+    if (filteredFileData.factors && filteredFileData.factors.product_factors) {
+        let productFactors = filteredFileData.factors.product_factors;
+
+        Object.keys(productFactors).forEach(factorKey => {
+            let value = productFactors[factorKey].value;
+
+            if (value < min || value > max) {
+                delete productFactors[factorKey];
+            }
+        });
+    }
+
+    if (filteredFileData.factors && filteredFileData.factors.quality_aspects) {
+        let qualityAspects = filteredFileData.factors.quality_aspects;
+
+        Object.keys(qualityAspects).forEach(qualityKey => {
+            let value = qualityAspects[qualityKey].value;
+
+            if (value < min || value > max) {
+                delete qualityAspects[qualityKey];
+            }
+        });
+    }
+
+    if (filteredFileData.measures) {
+        let measures = filteredFileData.measures;
+
+        Object.keys(measures).forEach(measureKey => {
+            let value = measures[measureKey].value;
+
+            if (value < min || value > max) {
+                delete measures[measureKey];
+            }
+        });
+    }
+
+    if (filteredFileData.diagnostics) {
+        let diagnostics = filteredFileData.diagnostics;
+
+        Object.keys(diagnostics).forEach(diagnosticKey => {
+            let value = diagnostics[diagnosticKey].value;
+
+            if (value < min || value > max) {
+                delete diagnostics[diagnosticKey];
+            }
+        });
+    }
+
+    return filteredFileData;
+
+
 }
