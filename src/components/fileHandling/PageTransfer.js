2fd688f911e89b2ec57dc08a4939b7d91cb62e9b
ziyi zhang <ziyi.zhang2@wsu.edu>
2023-05-26 18:53:50 -0700

    formatting the code

--- src/components/fileHandling/PageTransfer.js
+++ src/components/fileHandling/PageTransfer.js
@@ -11,139 +11,135 @@ const legendData = [
     { color: "yellow", range: "0.4 - 0.6", category: "Elevated", colorCode: "#fde101" },
     { color: "orange", range: "0.6 - 0.8", category: "High", colorCode: "#ff6500" },
     { color: "red", range: "0.8 - 1", category: "Severe", colorCode: "#cb0032" }
-  ];
-  
-  function getColor(value) {
+];
+
+function getColor(value) {
     if (value >= 0 && value <= 0.2) {
-      return "green";
+        return "green";
     } else if (value > 0.2 && value <= 0.4) {
-      return "blue";
+        return "blue";
     } else if (value > 0.4 && value <= 0.6) {
-      return "yellow";
+        return "yellow";
     } else if (value > 0.6 && value <= 0.8) {
-      return "orange";
+        return "orange";
     } else if (value > 0.8 && value <= 1) {
-      return "red";
+        return "red";
     } else {
-      return "black"; // Default color if value is outside the defined ranges
+        return "black"; // Default color if value is outside the defined ranges
     }
-  }
+}
 
 export default function PageTransfer(props) {
-  const { fileData } = props;
-  const [showSortOptions, setShowSortOptions] = useState(false);
-  const [sortedData, setSortedData] = useState(null);
-  const [filteredData, setFilteredData] = useState(null);
-  const [filteredRangeData, setFilteredRangeData] = useState(null);
+    const { fileData } = props;
+    const [showSortOptions, setShowSortOptions] = useState(false);
+    const [sortedData, setSortedData] = useState(null);
+    const [filteredData, setFilteredData] = useState(null);
+    const [filteredRangeData, setFilteredRangeData] = useState(null);
 
-  const renderTreeNode = (node) => {
-    const value = node.value || 0; // If value is not available, default to 0
-    const color = getColor(value);
-    return (
-      <div className="tree-node" style={{ backgroundColor: color }}>
-        <span className="node-name">{node.name}</span>
-        {/* Render child nodes recursively */}
-        {node.children && node.children.map((child) => renderTreeNode(child))}
-      </div>
-    );
-  };
+    const renderTreeNode = (node) => {
+        const value = node.value || 0; // If value is not available, default to 0
+        const color = getColor(value);
+        return (
+            <div className="tree-node" style={{ backgroundColor: color }}>
+                <span className="node-name">{node.name}</span>
+                {/* Render child nodes recursively */}
+                {node.children && node.children.map((child) => renderTreeNode(child))}
+            </div>
+        );
+    };
 
-  function renderLegend() {
-    return (
-      <div className="legend">
-        <h3>Legend</h3>
-        {legendData.map((item) => (
-          <div key={item.color}>
-            <span className="legend-color" style={{ backgroundColor: item.color }}></span>
-            <span className="legend-range">{item.range}</span>
-            <span className="legend-category">{item.category}</span>
-          </div>
-        ))}
-      </div>
-    );
-  }
-
-  const handleSort = (sortType) => {
-    let sorted;
-    if (sortType === "asc") {
-      sorted = sortASC(filteredData || fileData);
-    } else if (sortType === "desc") {
-      sorted = sortDESC(filteredData || fileData);
+    function renderLegend() {
+        return (
+            <div className="legend">
+                <h3>Legend</h3>
+                {legendData.map((item) => (
+                    <div key={item.color}>
+                        <span className="legend-color" style={{ backgroundColor: item.color }}></span>
+                        <span className="legend-range">{item.range}</span>
+                        <span className="legend-category">{item.category}</span>
+                    </div>
+                ))}
+            </div>
+        );
     }
-    setSortedData(sorted);
-    setFilteredData(null);
-    setFilteredRangeData(null);
-  };
 
-  const handleCategoryFilter = () => {
-    const filtered = filterByCategory(fileData);
-    setFilteredData(filtered);
-    setSortedData(null);
-    setFilteredRangeData(null);
-  };
+    const handleSort = (sortType) => {
+        let sorted;
+        if (sortType === "asc") {
+            sorted = sortASC(filteredData || fileData);
+        } else if (sortType === "desc") {
+            sorted = sortDESC(filteredData || fileData);
+        }
+        setSortedData(sorted);
+        setFilteredData(null);
+        setFilteredRangeData(null);
+    };
 
-  const handleFilterRange = () => {
-    const min = parseFloat(prompt("Enter the minimum value:"));
-    const max = parseFloat(prompt("Enter the maximum value:"));
-    const filtered = filterRange(fileData, min, max);
-    setFilteredRangeData(filtered);
-    setFilteredData(null);
-    setSortedData(null);
-  };
+    const handleCategoryFilter = () => {
+        const filtered = filterByCategory(fileData);
+        setFilteredData(filtered);
+        setSortedData(null);
+        setFilteredRangeData(null);
+    };
 
-  const handleReset = () => {
-    setFilteredRangeData(null);
-    setFilteredData(null);
-    setSortedData(null);
-  };
+    const handleFilterRange = () => {
+        const min = parseFloat(prompt("Enter the minimum value:"));
+        const max = parseFloat(prompt("Enter the maximum value:"));
+        const filtered = filterRange(fileData, min, max);
+        setFilteredRangeData(filtered);
+        setFilteredData(null);
+        setSortedData(null);
+    };
 
-  useEffect(() => {
-    setSortedData(null);
-    setFilteredData(null);
-    setFilteredRangeData(null);
-  }, [fileData]);
+    const handleReset = () => {
+        setFilteredRangeData(null);
+        setFilteredData(null);
+        setSortedData(null);
+    };
 
-  return (
-    <div className="unselectableText">
-      <div>
-        <div className="sort-dropdown">
-          <button className="sort-btn" onClick={() => setShowSortOptions(!showSortOptions)}>
-            Sort
-          </button>
-          {showSortOptions && (
-            <div className="sort-options">
-              <button className="sort-option" onClick={() => handleSort("asc")}>
-                Ascending
-              </button>
-              <button className="sort-option" onClick={() => handleSort("desc")}>
-                Descending
-              </button>
+    useEffect(() => {
+        setSortedData(null);
+        setFilteredData(null);
+        setFilteredRangeData(null);
+    }, [fileData]);
+
+    return (
+        <div className="unselectableText">
+            <div>
+                <div className="sort-dropdown">
+                    <button className="sort-btn" onClick={() => setShowSortOptions(!showSortOptions)}>
+                        Sort
+                    </button>
+                    {showSortOptions && (
+                        <div className="sort-options">
+                            <button className="sort-option" onClick={() => handleSort("asc")}>
+                                Ascending
+                            </button>
+                            <button className="sort-option" onClick={() => handleSort("desc")}>
+                                Descending
+                            </button>
+                        </div>
+                    )}
+                </div>
+                <button className="filter-btn" onClick={handleCategoryFilter}> Filter (Category) </button>
+                <button className="filter-btn" onClick={handleFilterRange}> Filter (Range)</button>
+                <button onClick={handleReset}>Reset Sorting & Filtering</button>
             </div>
-          )}
-        </div>
-        <button className="filter-btn" onClick={handleCategoryFilter}>
-          Category Filter
-        </button>
-        <button className="filter-btn" onClick={handleFilterRange}>
-          Range Filter (Min-Max)
-        </button>
-        <button onClick={handleReset}>Reset Sorting & Filtering</button>
-      </div>
-      <div className="legend-container">
-        <div className="legend">
-          <h3 className="legend-title">Risk Level</h3>
-          {legendData.map((item) => (
-            <div className="legend-item" key={item.color}>
-              <span className={`legend-color legend-color-${item.color}`} />
-              <span className="legend-range">{item.range}</span>
-              <span className="legend-category">{item.category}</span>
+            <div className="legend-container">
+                <div className="legend">
+                    <h3 className="legend-title">Risk Level</h3>
+                    {legendData.map((item) => (
+                        <div className="legend-item" key={item.color}>
+                            <span className={`legend-color legend-color-${item.color}`} />
+                            <span className="legend-range">{item.range}</span>
+                            <span className="legend-category">{item.category}</span>
+                        </div>
+                    ))}
+                </div>
             </div>
-          ))}
+            <TreeDisplay
+                fileData={sortedData || filteredData || filteredRangeData || fileData}
+            />
         </div>
-      </div>
-      <TreeDisplay
-        fileData={sortedData || filteredData || filteredRangeData || fileData}
-      />
-    </div>
-  );
+    );
 }
