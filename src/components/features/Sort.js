83fa1920a141aa39eb0aaf956f59c1d6ddc940c9
ziyi zhang <ziyi.zhang2@wsu.edu>
2023-10-23 10:49:12 -0700

    update the sorting based on node values descending

--- src/components/features/Sort.js
+++ src/components/features/Sort.js
@@ -84,47 +84,56 @@ export function sortASCforValues(fileData) {
 
 export function sortDESCforValues(fileData) {
     if (!fileData) {
-        return fileData;  // If fileData or factors is not defined, return original data
+        return fileData;  // If fileData is not defined, return original data
     }
 
     let sortedFileData = JSON.parse(JSON.stringify(fileData));
 
-    let dataArray_product_factors = fileData.factors.product_factors ? Object.values(sortedFileData.factors.product_factors) : [];
-    console.log(dataArray_product_factors);
-    let dataArray_quality_aspects = fileData.factors.quality_aspects ? Object.values(sortedFileData.factors.quality_aspects) : [];
-    let dataArray_measures = sortedFileData.measures ? Object.values(sortedFileData.measures) : [];
-    let dataArray_diagnostics = sortedFileData.diagnostics ? Object.values(sortedFileData.diagnostics) : [];
+    // Sort measures based on their "value" in descending order
+    if (sortedFileData.measures) {
+        let dataArray_measures = Object.values(sortedFileData.measures);
+        dataArray_measures.sort((a, b) => b.value - a.value);
+        sortedFileData.measures = {};
+        dataArray_measures.forEach(item => {
+            sortedFileData.measures[item.name] = item;
+        });
+    }
 
-    // Sort the array based on the "value" property in ascending order
-    dataArray_product_factors.sort((a, b) => b.value - a.value);
-    dataArray_quality_aspects.sort((a, b) => b.value - a.value);
-    dataArray_measures.sort((a, b) => b.value - a.value);
-    dataArray_diagnostics.sort((a, b) => b.value - a.value);
+    // Sort product_factors based on their "value" in descending order
+    if (sortedFileData.factors && sortedFileData.factors.product_factors) {
+        let dataArray_product_factors = Object.values(sortedFileData.factors.product_factors);
+        dataArray_product_factors.sort((a, b) => b.value - a.value);
+        sortedFileData.factors.product_factors = {};
+        dataArray_product_factors.forEach(item => {
+            sortedFileData.factors.product_factors[item.name] = item;
+        });
+    }
 
-    sortedFileData.factors.product_factors = {};
-    sortedFileData.factors.quality_aspects = {};
-    sortedFileData.measures = {};
-    sortedFileData.diagnostics = {};
+    // Sort quality_aspects based on their "value" in descending order
+    if (sortedFileData.factors && sortedFileData.factors.quality_aspects) {
+        let dataArray_quality_aspects = Object.values(sortedFileData.factors.quality_aspects);
+        dataArray_quality_aspects.sort((a, b) => b.value - a.value);
+        sortedFileData.factors.quality_aspects = {};
+        dataArray_quality_aspects.forEach(item => {
+            sortedFileData.factors.quality_aspects[item.name] = item;
+        });
+    }
 
-    dataArray_product_factors.forEach(item => {
-        sortedFileData.factors.product_factors[item.name] = item;
-    });
-    dataArray_quality_aspects.forEach(item => {
-        sortedFileData.factors.quality_aspects[item.name] = item;
-    });
-    dataArray_measures.forEach(item => {
-        sortedFileData.measures[item.name] = item;
-    });
-    dataArray_diagnostics.forEach(item => {
-        sortedFileData.diagnostics[item.name] = item;
-    });
+    // Sort tqi and align its weights in descending order
+    if (sortedFileData.factors && sortedFileData.factors.tqi) {
+        let dataArray_tqi = Object.values(sortedFileData.factors.tqi);
+        dataArray_tqi.sort((a, b) => b.value - a.value);
+        sortedFileData.factors.tqi = {};
+        dataArray_tqi.forEach(item => {
+            sortedFileData.factors.tqi[item.name] = item;
+        });
+    }
 
-
-    console.log("Descending Sorting Done! Largest <---> Smallest")
-
+    console.log("Descending Sorting Done! Largest <---> Smallest");
 
     return sortedFileData;
 }
+
 
 // based on weights
 
