9e2aa246c6c77ba37589ca8c54e6547a15b85c35
ziyi zhang <ziyi.zhang2@wsu.edu>
2023-12-29 14:31:18 -0800

    add a button container and corresponding buttons

---
+++ src/composites/FeaturesContainer/ButtonContainer.tsx
@@ -0,0 +1,43 @@
+import { useAtom, useAtomValue } from "jotai";
+import { State } from "../../state";
+import { Select, DropdownMenu, Separator, Flex, Text } from "@radix-ui/themes";
+import { SortButton} from "./SortButton";
+import { FilterButton } from "./FilterButton";
+import { ResetButton } from "./ResetButton";
+import { DesStatButton } from "./DescriptiveStatisticButton";
+import { QuickActionButton } from "./QuickActionButton";
+
+
+export const ButtonContainer = () => {
+  const dataset = useAtomValue(State.dataset);
+
+  return (
+      <Flex gap="3" align="center">
+
+        {/* Quick Actions */}
+        <QuickActionButton />
+        <Separator orientation="vertical" />
+
+        {/* Sorting */}
+        <SortButton />
+        <Separator orientation="vertical" />
+
+        {/* Filtering */}
+        <FilterButton />
+
+        <Separator orientation="vertical" />
+        {/* reset */}
+        <ResetButton />
+
+        <Separator orientation="vertical" />
+        {/*  descriptive statisitcs*/}
+        <DesStatButton />
+
+        {/* Others */}
+      </Flex>
+
+
+
+
+  );
+};
--- src/composites/TreeDisplay/Wrapper.tsx
+++ src/composites/TreeDisplay/Wrapper.tsx
@@ -1,14 +1,28 @@
 import { useAtom, useAtomValue } from "jotai";
 import { State } from "../../state";
 import { TreeDisplay } from "./TreeDisplay";
-import { Box } from "@radix-ui/themes";
+import { Box} from "@radix-ui/themes";
+import {ButtonContainer} from "../FeaturesContainer/ButtonContainer"
 
 export const Wrapper = () => {
   const dataset = useAtomValue(State.dataset);
 
   return (
-    <Box width="100%">
-      <TreeDisplay fileData={dataset} />
-    </Box>
+    <div>
+      {/* Title */}
+      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '20px' }}>
+        <img src="https://www.cisa.gov/profiles/cisad8_gov/themes/custom/gesso/dist/images/backgrounds/6fdaa25709d28dfb5cca.svg" alt="CISA Logo" width="100" height="100" style={{ marginRight: '20px' }} />
+        <h1>PIQUE Visualizer</h1>
+      </div>
+
+      <Box width={"100%"}>
+        <ButtonContainer />
+      </Box>
+
+
+      <Box width="100%">
+        <TreeDisplay fileData={dataset} />
+      </Box>
+    </div>
   );
 };
