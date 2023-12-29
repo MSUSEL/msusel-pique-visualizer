80e34ce0aa75d29a807a33d093e4eaf6c60296fd
ziyi zhang <ziyi.zhang2@wsu.edu>
2023-12-29 10:22:08 -0800

    Updated FileUploader component to display title, icon and button at the center

--- src/composites/FileUploader/FileUploader.tsx
+++ src/composites/FileUploader/FileUploader.tsx
@@ -4,39 +4,52 @@ import * as schema from "../../data/schema";
 import { useSetAtom } from "jotai";
 import { State } from "../../state";
 
-export interface FileUploaderProps {}
+
+export interface FileUploaderProps { }
 
 export const FileUploader = () => {
   const [_, selectFile] = useFileUpload();
   const setDataset = useSetAtom(State.dataset);
+
   return (
-    <Box>
-      <Button
-        onClick={() => {
-          selectFile({ accept: "json", multiple: false }, ({ file }) => {
-            const fileReader = new FileReader();
-            fileReader.onload = (e) => {
-              const result = e.target?.result;
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', width: '100vw' }}>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img src="https://www.cisa.gov/profiles/cisad8_gov/themes/custom/gesso/dist/images/backgrounds/6fdaa25709d28dfb5cca.svg" alt="CISA Logo" width="100" height="100" style={{ marginRight: '20px' }} />
+        <h1>PIQUE Visualizer</h1>
+      </div>
+      <p>Please upload the PIQUE JSON file to visualize the results</p>
 
-              try {
-                const parsed = JSON.parse(result as string);
-                const validationResult = schema.base.dataset.safeParse(parsed);
+      <Box position={"relative"}>
+        <Button
+          size="4"
+          variant="surface"
+          radius="large"
+          onClick={() => {
+            selectFile({ accept: "json", multiple: false }, ({ file }) => {
+              const fileReader = new FileReader();
+              fileReader.onload = (e) => {
+                const result = e.target?.result;
 
-                if (validationResult.success) {
-                  setDataset(validationResult.data);
-                } else {
-                  // error
+                try {
+                  const parsed = JSON.parse(result as string);
+                  const validationResult = schema.base.dataset.safeParse(parsed);
+
+                  if (validationResult.success) {
+                    setDataset(validationResult.data);
+                  } else {
+                    // error
+                  }
+                } catch (e) {
+                  //error
                 }
-              } catch (e) {
-                //error
-              }
-            };
-            fileReader.readAsText(file);
-          });
-        }}
-      >
-        Select File
-      </Button>
-    </Box>
+              };
+              fileReader.readAsText(file);
+            });
+          }}
+        >
+          Select File
+        </Button>
+      </Box>
+    </div>
   );
 };
