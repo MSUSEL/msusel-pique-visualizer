c28fdecba469500d5387779d43faf0b6b6e1b132
ziyi zhang <ziyi.zhang2@wsu.edu>
2023-12-29 14:34:38 -0800

    centerlize the title and icon

--- src/composites/TreeDisplay/Wrapper.tsx
+++ src/composites/TreeDisplay/Wrapper.tsx
@@ -1,16 +1,16 @@
-import { useAtom, useAtomValue } from "jotai";
+import { useAtomValue } from "jotai";
 import { State } from "../../state";
 import { TreeDisplay } from "./TreeDisplay";
-import { Box} from "@radix-ui/themes";
-import {ButtonContainer} from "../FeaturesContainer/ButtonContainer"
+import { Box } from "@radix-ui/themes";
+import { ButtonContainer } from "../FeaturesContainer/ButtonContainer";
 
 export const Wrapper = () => {
   const dataset = useAtomValue(State.dataset);
 
   return (
     <div>
-      {/* Title */}
-      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '20px' }}>
+      {/* Title and Icon Centered */}
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px' }}>
         <img src="https://www.cisa.gov/profiles/cisad8_gov/themes/custom/gesso/dist/images/backgrounds/6fdaa25709d28dfb5cca.svg" alt="CISA Logo" width="100" height="100" style={{ marginRight: '20px' }} />
         <h1>PIQUE Visualizer</h1>
       </div>
@@ -18,7 +18,6 @@ export const Wrapper = () => {
       <Box width={"100%"}>
         <ButtonContainer />
       </Box>
-
 
       <Box width="100%">
         <TreeDisplay fileData={dataset} />
