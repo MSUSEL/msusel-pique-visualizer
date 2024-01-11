79b4b225f62d359ee7a94fde089dd4c5b9a90fab
ziyi zhang <ziyi.zhang2@wsu.edu>
2024-01-11 09:27:05 -0800

    migrated the reset feature

--- src/composites/FeaturesContainer/ResetButton.tsx
+++ src/composites/FeaturesContainer/ResetButton.tsx
@@ -1,23 +1,34 @@
-import { useAtom, useAtomValue } from "jotai";
+import { useSetAtom } from "jotai";
 import { State } from "../../state";
 import { Flex, Button } from "@radix-ui/themes";
 import { ResetIcon } from "@radix-ui/react-icons";
 
 export const ResetButton = () => {
-    const dataset = useAtomValue(State.dataset);
+    // Get the setter functions for each state
+    const setSortingState = useSetAtom(State.sortingState);
+    const setFilteringState = useSetAtom(State.filteringState);
+    const setHideZeroWeightEdgeState = useSetAtom(State.hideZeroWeightEdgeState);
+    const setFilteringByRiskLevelCheckboxStates = useSetAtom(State.filteringByRiskLevelCheckboxStates);
+
+    // Define a function to handle the reset action
+    const handleReset = () => {
+        setSortingState("no-sort");
+        setFilteringState("no-filter");
+        setHideZeroWeightEdgeState("not-hidding");
+        setFilteringByRiskLevelCheckboxStates({
+            Insignificant: true,
+            Low: true,
+            Medium: true,
+            High: true,
+            Severe: true,
+        });
+    };
 
     return (
-
         <Flex gap="3" align="center">
-
-            <Button size="2" variant="soft" highContrast>
-                <ResetIcon width="18" height="18" />Reset
+            <Button size="2" variant="soft" highContrast onClick={handleReset}>
+                <ResetIcon width="18" height="18" /> Reset
             </Button>
-
         </Flex>
-
-
-
-
     );
 };
