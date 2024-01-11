ab7dc044018a9dc120e40029d693b5c6e20e7dce
ziyi zhang <ziyi.zhang2@wsu.edu>
2024-01-11 09:20:15 -0800

    add hover card to sorting to example how the sorting resukts should look like in a tree view

--- src/composites/FeaturesContainer/SortButton.tsx
+++ src/composites/FeaturesContainer/SortButton.tsx
@@ -1,6 +1,6 @@
 import { useAtom } from "jotai";
 import { State } from "../../state";
-import { Select, Flex } from "@radix-ui/themes";
+import { Select, Flex, HoverCard, Link, Text } from "@radix-ui/themes";
 import { CaretSortIcon } from "@radix-ui/react-icons";
 
 export const SortButton = () => {
@@ -24,17 +24,64 @@ export const SortButton = () => {
         <Select.Trigger placeholder="No Sorting" variant="soft" />
         <Select.Content>
           <Select.Group>
-            <Select.Label>Values</Select.Label>
-            <Select.Item value="value-asc">Value - Ascending</Select.Item>
-            <Select.Item value="value-desc">Value - Descending</Select.Item>
+            <Select.Label>Based on Values</Select.Label>
+            <Select.Item value="value-asc"> Value - {' '}
+              <HoverCard.Root>
+                <HoverCard.Trigger>
+                  <Link href="#">Ascending</Link>
+                </HoverCard.Trigger>
+                <HoverCard.Content size="2">
+                  <Text as="div" size="2" style={{ maxWidth: 325 }}>
+                    Ascending: For the tree view, the ascending sorting will sort the smallest on the right and the largest on the right.
+                  </Text>
+                </HoverCard.Content>
+
+              </HoverCard.Root>
+              {' '}
+            </Select.Item>
+            <Select.Item value="value-desc">Value - {' '}
+              <HoverCard.Root>
+                <HoverCard.Trigger>
+                  <Link href="#">Descending</Link>
+                </HoverCard.Trigger>
+                <HoverCard.Content size="2">
+                  <Text as="div" size="2" style={{ maxWidth: 325 }}>
+                    Descending: For the tree view, the descending sorting will sort the largest on the right and the smallest on the right.
+                  </Text>
+                </HoverCard.Content>
+
+              </HoverCard.Root>
+              {' '}</Select.Item>
           </Select.Group>
 
           <Select.Separator />
 
           <Select.Group>
-            <Select.Label>Weights</Select.Label>
-            <Select.Item value="weight-asc">Weight - Ascending</Select.Item>
-            <Select.Item value="weight-desc">Weight - Descending</Select.Item>
+            <Select.Label>Based on Weights</Select.Label>
+            <Select.Item value="weight-asc">Weight - {' '}
+              <HoverCard.Root>
+                <HoverCard.Trigger>
+                  <Link href="#">Ascending</Link>
+                </HoverCard.Trigger>
+                <HoverCard.Content size="2">
+                  <Text as="div" size="2" style={{ maxWidth: 325 }}>
+                    Ascending: For the tree view, the ascending sorting will sort the smallest on the right and the largest on the right.
+                  </Text>
+                </HoverCard.Content>
+
+              </HoverCard.Root>
+              {' '}</Select.Item>
+            <Select.Item value="weight-desc">Weight - <HoverCard.Root>
+              <HoverCard.Trigger>
+                <Link href="#">Descending</Link>
+              </HoverCard.Trigger>
+              <HoverCard.Content size="2">
+                <Text as="div" size="2" style={{ maxWidth: 325 }}>
+                  Descending: For the tree view, the descending sorting will sort the largest on the right and the smallest on the right.
+                </Text>
+              </HoverCard.Content>
+
+            </HoverCard.Root></Select.Item>
           </Select.Group>
         </Select.Content>
       </Select.Root>
