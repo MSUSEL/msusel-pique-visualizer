98cee611c084dc21a6e835882e8dce845e103d28
sockomode <eli123013@gmail.com>
2024-01-14 21:44:03 -0800

    removed auto alphabetical order for now

--- src/composites/TreeDisplay/nodeDescriptionPanel/NodeDescriptionPanel.tsx
+++ src/composites/TreeDisplay/nodeDescriptionPanel/NodeDescriptionPanel.tsx
@@ -1,35 +1,30 @@
-import "./NodeDescriptionPanel.css"
-import {useEffect, useState} from "react";
-import {determineNodeInfo} from "./NodeDescriptionPanelHelpers";
+import "./NodeDescriptionPanel.css";
+import { useEffect, useState } from "react";
+import { determineNodeInfo } from "./NodeDescriptionPanelHelpers";
 
-export default function NodeDescriptionPanel(props) {
-
-    const [nodes,setNodes] = useState([]);
-
-    // Rerender this component whenever the nodes prop from TreeDisplay changes.
-    useEffect(() => {
-        setNodes(props.nodes)
-    },[props.nodes])
-
-    function determinePanelHeight() {
-        const height = 100/nodes.length;
-        if (height > 34) return 33.33;
-        return height;
-    }
-
-    function makeNodePanelRectangles() {
-        return (nodes.map( (node,i = node.hashCode()) => (
-                <div className={`${i === nodes.length-1 ? (nodes.length > 2 ? "node-bottom-panel" : "node-panel") : "node-panel"}`} style={{height : `${determinePanelHeight()}%`}} key={i}>
-                    {determineNodeInfo(node,props.impacts)}
-                </div>
-                )
-            )
-        )
-    }
-
-    return (
-        <div id = "node_description_panel">
-            {makeNodePanelRectangles()}
-        </div>
-    )
+export default function NodeDescriptionPanel(props: { nodes: any[], impacts: any }) {
+  const [nodes, setNodes] = useState<any[]>([]);
+  
+  // Rerender this component whenever the nodes prop from TreeDisplay changes.
+  useEffect(() => {
+    setNodes(props.nodes);
+  }, [props.nodes]);
+  
+  function makeNodePanelRectangles() {
+    return nodes.map((node, i) => (
+      <div
+        className={`${i === nodes.length - 1 ? (nodes.length > 2 ? "node-bottom-panel" : "node-panel") : "node-panel"}`}
+        key={i}
+      >
+        {determineNodeInfo(node, props.impacts)}
+      </div>
+    ));
+  }
+  
+  return (
+    <div id="node_description_panel" className="scrollable-panel">
+      {makeNodePanelRectangles()}
+    </div>
+  );
 }
+
