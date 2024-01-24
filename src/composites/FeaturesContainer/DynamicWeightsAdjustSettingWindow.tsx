bdd86b50e643eed1fc2f0df42ea4f6a8ac36eaa5
ziyi zhang <ziyi.zhang2@wsu.edu>
2024-01-23 21:01:08 -0800

    add the close button

--- src/composites/FeaturesContainer/DynamicWeightsAdjustSettingWindow.tsx
+++ src/composites/FeaturesContainer/DynamicWeightsAdjustSettingWindow.tsx
@@ -1,8 +1,8 @@
 import { useAtom, useAtomValue } from "jotai";
 import { State } from "../../state";
-import { CharacteristicsTableGenerator } from "../DynamicWeightsAdjustment/CharacteristicsTable"; 
-import { Button, Dialog, Flex, Text, HoverCard, Link, Strong, Callout, Box, Inset } from "@radix-ui/themes";
-import { InfoCircledIcon, GearIcon } from "@radix-ui/react-icons";
+import { CharacteristicsTableGenerator } from "../DynamicWeightsAdjustment/CharacteristicsTable";
+import { Button, Dialog, Flex, Text, HoverCard, Link, Strong, Callout, Box, Inset, IconButton } from "@radix-ui/themes";
+import { InfoCircledIcon, GearIcon, CrossCircledIcon, Cross1Icon } from "@radix-ui/react-icons";
 
 export const DynamicWeightsButton = () => {
     return (
@@ -43,29 +43,31 @@ export const DynamicWeightsButton = () => {
             <Box>
                 <Dialog.Root>
                     <Dialog.Trigger>
-                        <Button size="2" variant="surface" >
-                            Adjust quality characheristics weights
+                        <Button size="2" variant="surface">
+                            Adjust quality characteristics weights
                         </Button>
                     </Dialog.Trigger>
-                    <Dialog.Content>
+                    <Dialog.Content style={{ position: 'relative' }}>
                         <Dialog.Title>Characteristics and corresponding weights</Dialog.Title>
                         <Dialog.Description>
-                            
+                            {/* Dialog description content */}
                         </Dialog.Description>
+
+                        {/* Position the close button absolutely within the Dialog.Content */}
+                        <Dialog.Close style={{ position: 'absolute', top: '10px', right: '10px' }}>
+                            <IconButton radius="full" color="gray">
+                                <Cross1Icon width="15" height="15" />
+                            </IconButton>
+                        </Dialog.Close>
 
                         <Inset side="x" my="5">
                             <CharacteristicsTableGenerator />
                         </Inset>
 
-                        <Flex gap="3" justify="end">
-                            <Dialog.Close>
-                                <Button variant="soft" color="gray">
-                                    Close
-                                </Button>
-                            </Dialog.Close>
-                        </Flex>
+                        {/* Other dialog content */}
                     </Dialog.Content>
                 </Dialog.Root>
+
             </Box>
 
         </Flex>
