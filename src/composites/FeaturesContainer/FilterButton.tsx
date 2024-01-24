3ebb402d58f4a11629e5ad3c8815891c10949dfe
ziyi zhang <ziyi.zhang2@wsu.edu>
2024-01-24 10:17:46 -0800

    update the layout of left button bar - filtering section

--- src/composites/FeaturesContainer/FilterButton.tsx
+++ src/composites/FeaturesContainer/FilterButton.tsx
@@ -1,6 +1,6 @@
 import { useAtom, useAtomValue } from "jotai";
 import { State } from "../../state";
-import { Callout, HoverCard, Dialog, Button, Flex, Text, DropdownMenu, Link, Strong, Em, Heading } from "@radix-ui/themes";
+import { Callout, HoverCard, Dialog, Button, Flex, Text, DropdownMenu, Link, Strong, Em, Heading, Box } from "@radix-ui/themes";
 import { MixerHorizontalIcon, CheckCircledIcon, SliderIcon } from "@radix-ui/react-icons";
 import * as React from "react";
 import * as Slider from '@radix-ui/react-slider';
@@ -26,7 +26,7 @@ export const FilterButton = () => {
   const [localMinValue, setLocalMinValue] = React.useState(0);
   const [localMaxValue, setLocalMaxValue] = React.useState(1);
   const [localMinWeight, setLocalMinWeight] = React.useState(0);
-  const [localMaxWeight, setLocalMaxWeight] = React.useState(1); 
+  const [localMaxWeight, setLocalMaxWeight] = React.useState(1);
 
   // Atoms from state.ts
   const [, setMinValueState] = useAtom(State.minValueState);
@@ -104,180 +104,170 @@ export const FilterButton = () => {
 
   return (
 
-    <Flex gap="3" align="center">
-      <div style={{ width: '100%', maxWidth: 300, margin: '0 15px' }}>
-        {/* Title */}
-        <div className="Text" style={{ fontWeight: 500 }}>
-          <Text>
-            <HoverCard.Root>
-              <HoverCard.Trigger>
-                <Link href="#" size='3'>
-                  <MixerHorizontalIcon /> Filtering
-                </Link>
+    <Flex gap="3" align={"start"} direction={"column"}>
 
-              </HoverCard.Trigger>
-              <HoverCard.Content>
-                <Text as="div" size="1" style={{ maxWidth: 325 }}>
-                  <Strong>Filter</Strong> the evaluation results, consisting of quality characteristics, factors, measures and diagnostics,
-                  based on their <Em>risk levels</Em>, <Em> values range </Em>, or <Em> weights range </Em>.
+      {/* Title */}
+      <Box>
+        <HoverCard.Root>
+          <HoverCard.Trigger>
+            <Link href="#" size='3'><MixerHorizontalIcon /> Filtering </Link>
+          </HoverCard.Trigger>
+          <HoverCard.Content>
+            <Text as="div" size="1" style={{ maxWidth: 325 }}>
+              <Strong>Filter</Strong> the evaluation results, consisting of quality characteristics, factors, measures and diagnostics,
+              based on their <Em>risk levels</Em>, <Em> values range </Em>, or <Em> weights range </Em>.
+            </Text>
+          </HoverCard.Content>
+        </HoverCard.Root>
+      </Box>
 
-                </Text>
-              </HoverCard.Content>
-            </HoverCard.Root>
-          </Text>
-        </div>
+      {/* Filter summary */}
+      <Box style={{width:'100%'}} >
+        <Callout.Root>
+          <Callout.Icon><CheckCircledIcon /></Callout.Icon>
+          <Callout.Text align={"left"}>
+            <Strong>Current Filtering Criteria: </Strong> {generateFilterSummary()} 
+          </Callout.Text>
+        </Callout.Root>
+      </Box>
 
-        {/* Filter summary */}
-        <div>
-          <Callout.Root>
-            <Callout.Icon>
-              <CheckCircledIcon />
-            </Callout.Icon>
-            <Callout.Text>
-              <Strong>
-                Current Filtering Criteria
-              </Strong>
-              {generateFilterSummary()}
-            </Callout.Text>
-          </Callout.Root>
+      {/* Filter by risk level */}
+      <Flex direction={"column"} align={"start"}>
+      <Box style={{width:'100%'}}>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger >
+            <Button variant="surface" style={{ width: '100%', margin: '0 15px' }} >
+              <MixerHorizontalIcon /> Filter by Risk Level
+            </Button>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            {/* filter by risk level*/}
+            <DropdownMenu.Group >
+              <DropdownMenu.Label>Risk Level</DropdownMenu.Label>
+              {["Insignificant", "Low", "Medium", "High", "Severe"].map((label) => (
+                <DropdownMenu.Item key={label} >
+                  <label>
+                    <input
+                      type="checkbox"
+                      checked={checkboxStates[label]}
+                      onChange={handleCheckboxChange(label)}
+                    />
+                    {label}
+                  </label>
+                </DropdownMenu.Item>
+              ))}
+            </DropdownMenu.Group>
 
-        </div>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      </Box>
 
-        {/* Filter by risk level */}
-        <div>
-          <DropdownMenu.Root>
-            <DropdownMenu.Trigger >
-              <Button variant="surface" style={{ width: '100%', maxWidth: 300, margin: '0 15px' }}>
-                <MixerHorizontalIcon /> Filter by Risk Level
-              </Button>
-            </DropdownMenu.Trigger>
-            <DropdownMenu.Content>
-              {/* filter by risk level*/}
-              <DropdownMenu.Group>
-                <DropdownMenu.Label>Risk Level</DropdownMenu.Label>
-                {["Insignificant", "Low", "Medium", "High", "Severe"].map((label) => (
-                  <DropdownMenu.Item key={label}>
-                    <label>
-                      <input
-                        type="checkbox"
-                        checked={checkboxStates[label]}
-                        onChange={handleCheckboxChange(label)}
-                      />
-                      {label}
-                    </label>
-                  </DropdownMenu.Item>
-                ))}
-              </DropdownMenu.Group>
+      {/* filter by objects value range*/}
+      <Box style={{width:'100%'}}>
+        <Dialog.Root>
+          <Dialog.Trigger >
+            <Button variant="surface" style={{ width: '100%', maxWidth: 300, margin: '0 15px' }}>
+              <SliderIcon /> Filter by Value Range
+            </Button>
+          </Dialog.Trigger>
+          <Dialog.Content style={{ maxWidth: '350px', width: '100%' }}>
+            <Dialog.Title>By value range</Dialog.Title>
+            <Dialog.Description size="2" mb="4">
+              Drag the slider to decide the minimum and maximum of value range.
+            </Dialog.Description>
 
-            </DropdownMenu.Content>
-          </DropdownMenu.Root>
-        </div>
-
-        {/* filter by objects value range*/}
-        <div>
-          <Dialog.Root>
-            <Dialog.Trigger >
-              <Button variant="surface" style={{ width: '100%', maxWidth: 300, margin: '0 15px' }}>
-                <SliderIcon /> Filter by Value Range
-              </Button>
-            </Dialog.Trigger>
-            <Dialog.Content style={{ maxWidth: '350px', width: '100%' }}>
-              <Dialog.Title>By value range</Dialog.Title>
-              <Dialog.Description size="2" mb="4">
-                Drag the slider to decide the minimum and maximum of value range.
-              </Dialog.Description>
-
-              <Flex direction="column" gap="3">
-                <Flex justify="between" align="center">
-                  <Text size="2">Min: {localMinValue.toFixed(1)}</Text>
-                  <Text size="2">Max: {localMaxValue.toFixed(1)}</Text>
-                </Flex>
-                <Slider.Root
-                  className="SliderRoot"
-                  defaultValue={[0, 1]}
-                  min={-1}
-                  max={1}
-                  step={0.1}
-                  value={[localMinValue, localMaxValue]}
-                  onValueChange={([min, max]) => {
-                    setLocalMinValue(min);
-                    setLocalMaxValue(max);
-                  }}
-                >
-                  <Slider.Track className="SliderTrack">
-                    <Slider.Range className="SliderRange" />
-                  </Slider.Track>
-                  <Slider.Thumb className="SliderThumb" />
-                  <Slider.Thumb className="SliderThumb" />
-                </Slider.Root>
+            <Flex direction="column" gap="3">
+              <Flex justify="between" align="center">
+                <Text size="2">Min: {localMinValue.toFixed(1)}</Text>
+                <Text size="2">Max: {localMaxValue.toFixed(1)}</Text>
               </Flex>
+              <Slider.Root
+                className="SliderRoot"
+                defaultValue={[0, 1]}
+                min={-1}
+                max={1}
+                step={0.1}
+                value={[localMinValue, localMaxValue]}
+                onValueChange={([min, max]) => {
+                  setLocalMinValue(min);
+                  setLocalMaxValue(max);
+                }}
+              >
+                <Slider.Track className="SliderTrack">
+                  <Slider.Range className="SliderRange" />
+                </Slider.Track>
+                <Slider.Thumb className="SliderThumb" />
+                <Slider.Thumb className="SliderThumb" />
+              </Slider.Root>
+            </Flex>
 
-              <Flex gap="3" mt="4" justify="end">
-                <Dialog.Close >
-                  <Button variant="soft" color="gray">Cancel</Button>
-                </Dialog.Close>
-                <Dialog.Close onClick={handleSaveValueRange}>
-                  <Button>Save</Button>
-                </Dialog.Close>
+            <Flex gap="3" mt="4" justify="end">
+              <Dialog.Close >
+                <Button variant="soft" color="gray">Cancel</Button>
+              </Dialog.Close>
+              <Dialog.Close onClick={handleSaveValueRange}>
+                <Button>Save</Button>
+              </Dialog.Close>
+            </Flex>
+          </Dialog.Content>
+        </Dialog.Root>
+      </Box>
+
+      {/* filter by objects weights range*/}
+      <Box style={{width:'100%'}}>
+        <Dialog.Root>
+          <Dialog.Trigger>
+            <Button variant="surface" style={{ width: '100%', maxWidth: 300, margin: '0 15px' }}>
+              <SliderIcon /> Filter by Weight Range
+            </Button>
+          </Dialog.Trigger>
+
+          <Dialog.Content style={{ maxWidth: '350px', width: '100%' }}>
+            <Dialog.Title>By weight range</Dialog.Title>
+            <Dialog.Description size="2" mb="4">
+              Drag the slider to decide the minimum and maximum of weight range.
+            </Dialog.Description>
+
+            <Flex direction="column" gap="3">
+              <Flex justify="between" align="center">
+                <Text size="2">Min: {localMinWeight.toFixed(1)}</Text>
+                <Text size="2">Max: {localMaxWeight.toFixed(1)}</Text>
               </Flex>
-            </Dialog.Content>
-          </Dialog.Root>
-        </div>
+              <Slider.Root
+                className="SliderRoot"
+                defaultValue={[0, 1]}
+                min={0}
+                max={1}
+                step={0.1}
+                value={[localMinWeight, localMaxWeight]}
+                onValueChange={([min, max]) => {
+                  setLocalMinWeight(min);
+                  setLocalMaxWeight(max);
+                }}
+              >
+                <Slider.Track className="SliderTrack">
+                  <Slider.Range className="SliderRange" />
+                </Slider.Track>
+                <Slider.Thumb className="SliderThumb" />
+                <Slider.Thumb className="SliderThumb" />
+              </Slider.Root>
+            </Flex>
 
-        {/* filter by objects weights range*/}
-        <div>
-          <Dialog.Root>
-            <Dialog.Trigger>
-              <Button variant="surface" style={{ width: '100%', maxWidth: 300, margin: '0 15px' }}>
-                <SliderIcon /> Filter by Weight Range
-              </Button>
-            </Dialog.Trigger>
+            <Flex gap="3" mt="4" justify="end">
+              <Dialog.Close>
+                <Button variant="soft" color="gray">
+                  Cancel
+                </Button>
+              </Dialog.Close>
+              <Dialog.Close onClick={handleSaveWeightRange}>
+                <Button>Save</Button>
+              </Dialog.Close>
+            </Flex>
+          </Dialog.Content>
+        </Dialog.Root>
+      </Box>
+      </Flex>
 
-            <Dialog.Content style={{ maxWidth: '350px', width: '100%' }}>
-              <Dialog.Title>By weight range</Dialog.Title>
-              <Dialog.Description size="2" mb="4">
-                Drag the slider to decide the minimum and maximum of weight range.
-              </Dialog.Description>
-
-              <Flex direction="column" gap="3">
-                <Flex justify="between" align="center">
-                  <Text size="2">Min: {localMinWeight.toFixed(1)}</Text>
-                  <Text size="2">Max: {localMaxWeight.toFixed(1)}</Text>
-                </Flex>
-                <Slider.Root
-                  className="SliderRoot"
-                  defaultValue={[0, 1]}
-                  min={0}
-                  max={1}
-                  step={0.1}
-                  value={[localMinWeight, localMaxWeight]}
-                  onValueChange={([min, max]) => {
-                    setLocalMinWeight(min);
-                    setLocalMaxWeight(max);
-                  }}
-                >
-                  <Slider.Track className="SliderTrack">
-                    <Slider.Range className="SliderRange" />
-                  </Slider.Track>
-                  <Slider.Thumb className="SliderThumb" />
-                  <Slider.Thumb className="SliderThumb" />
-                </Slider.Root>
-              </Flex>
-
-              <Flex gap="3" mt="4" justify="end">
-                <Dialog.Close>
-                  <Button variant="soft" color="gray">
-                    Cancel
-                  </Button>
-                </Dialog.Close>
-                <Dialog.Close onClick={handleSaveWeightRange}>
-                  <Button>Save</Button>
-                </Dialog.Close>
-              </Flex>
-            </Dialog.Content>
-          </Dialog.Root>
-        </div>
-      </div>
     </Flex>
 
   );
