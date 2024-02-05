31a4b875037f07f5e4135a60df1c5e6a8b9d7ae2
sockomode <eli123013@gmail.com>
2024-02-05 13:22:20 -0800

    removed extra re-rendering useEffect

--- src/composites/TreeDisplay/nodeDescriptionPanel/NodeDescriptionPanel.tsx
+++ src/composites/TreeDisplay/nodeDescriptionPanel/NodeDescriptionPanel.tsx
@@ -3,25 +3,19 @@ import { useEffect, useState, useMemo, useRef } from "react";
 import { determineNodeInfo } from "./NodeDescriptionPanelHelpers";
 
 export default function NodeDescriptionPanel(props: { nodes: any[]; impacts: any }) {
-  const [nodes, setNodes] = useState<any[]>([]);
   const [orderBy, setOrderBy] = useState<string>("default");
   const [orderDirection, setOrderDirection] = useState<string>("asc");
   const lastNodeRef = useRef<HTMLDivElement | null>(null);
-
-  // Rerender this component whenever the nodes prop from TreeDisplay changes.
-  useEffect(() => {
-    setNodes(props.nodes);
-  }, [props.nodes]);
 
   // Scroll to the last node whenever nodes change
   useEffect(() => {
     if (lastNodeRef.current) {
       lastNodeRef.current.scrollIntoView({ behavior: "smooth" });
     }
-  }, [nodes]);
+  }, [props.nodes]);
 
   const makeNodePanelRectangles = useMemo(() => {
-    let orderedNodes = [...nodes];
+    let orderedNodes = [...props.nodes];
 
     switch (orderBy) {
       case "default":
@@ -41,10 +35,10 @@ export default function NodeDescriptionPanel(props: { nodes: any[]; impacts: any
 
     return orderedNodes.map((node, i) => (
       <div
-        ref={i === nodes.length - 1 ? lastNodeRef : null}
+        ref={i === orderedNodes.length - 1 ? lastNodeRef : null}
         className={`${
-          orderBy === "default" && i === nodes.length - 1
-            ? nodes.length > 2
+          orderBy === "default" && i === orderedNodes.length - 1
+            ? orderedNodes.length > 2
               ? "node-bottom-panel highlight"
               : "node-panel highlight"
             : "node-panel"
@@ -54,7 +48,7 @@ export default function NodeDescriptionPanel(props: { nodes: any[]; impacts: any
         {determineNodeInfo(node, props.impacts)}
       </div>
     ));
-  }, [nodes, orderBy, orderDirection, props.impacts]);
+  }, [props.nodes, orderBy, orderDirection, props.impacts]);
 
   const handleOrderByChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setOrderBy(e.target.value);
@@ -70,7 +64,6 @@ export default function NodeDescriptionPanel(props: { nodes: any[]; impacts: any
       <select id="orderBy" value={orderBy} onChange={handleOrderByChange}>
         <option value="default">Insertion Order</option>
         <option value="alphabetical">Alphabetical Order</option>
-        {/* <option value="nodeType">Node Type Order</option> */}
         <option value="value">Value Order</option>
       </select>
 
