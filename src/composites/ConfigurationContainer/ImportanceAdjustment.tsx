1bd1ff48c6746788f48e51cb8f38776264a04653
ziyi zhang <ziyi.zhang2@wsu.edu>
2024-02-08 22:05:16 -0800

    maintain the state selectedProfile in the parent component

--- src/composites/ConfigurationContainer/ImportanceAdjustment.tsx
+++ src/composites/ConfigurationContainer/ImportanceAdjustment.tsx
@@ -1,116 +1,133 @@
 import { useAtom, useAtomValue } from "jotai";
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useState } from "react";
 import { State } from "../../state";
-import { Button, Flex, Text, HoverCard, Link, Strong, Callout, Box, Separator, IconButton, Select } from "@radix-ui/themes";
-import { InfoCircledIcon, GearIcon, Cross2Icon, Cross1Icon } from "@radix-ui/react-icons";
-import * as Dialog from '@radix-ui/react-dialog';
-import "../Style/Dialog.css"
+import {
+  Button,
+  Flex,
+  Text,
+  HoverCard,
+  Link,
+  Strong,
+  Callout,
+  Box,
+  Separator,
+  IconButton,
+  Select,
+} from "@radix-ui/themes";
+import {
+  InfoCircledIcon,
+  GearIcon,
+  Cross2Icon,
+  Cross1Icon,
+} from "@radix-ui/react-icons";
+import * as Dialog from "@radix-ui/react-dialog";
+import "../Style/Dialog.css";
 import { ProfileSelection } from "./ImportanceAdjustment/ProfileSelection";
 import { AdjustmentTable } from "./ImportanceAdjustment/AdjsutmentTable";
 import { Profile } from "../../types";
 
-
 export const ImportanceAdjustment = () => {
-    const [selectedProfile, setSelectedProfile] = useState<Profile | Profile[] | null>(null);
+  const [selectedProfile, setSelectedProfile] = useState<
+    Profile | Profile[] | null
+  >(null);
 
-    // haddle the profile selection and apply
-    // const [isProfileApplied, setIsProfileApplied] = useState(false);
+  // Handler that updates the selectedProfile state
+  const handleProfileApply = (profile: Profile[] | null) => {
+    setSelectedProfile(profile);
+  };
 
-    const handleProfileApply = (profile: Profile[] | null) => {
-        setSelectedProfile(profile);
-        // setIsProfileApplied(true);
-    };
+  const isProfileApplied = selectedProfile !== null;
 
-    const isProfileApplied = selectedProfile !== null;
+  const handleReset = () => {
+    setSelectedProfile(null); // Reset the selected profile when the user clicks reset
+  };
 
-    const handleReset = () => {
-        setSelectedProfile(null); // Reset the flag when the user clicks reset
-    };
+  return (
+    <Flex direction="column" gap="3" align="start">
+      {/* title, right now we will keep it as "weight" */}
+      <Box>
+        <HoverCard.Root>
+          <HoverCard.Trigger>
+            <Link href="#" size="3" style={{ margin: "0px" }}>
+              {" "}
+              <GearIcon /> Dynamic Importance Adjustment
+            </Link>
+          </HoverCard.Trigger>
+          <HoverCard.Content>
+            <Text as="div" size="1" style={{ maxWidth: 325 }}>
+              For Quality Chracteristics, <Strong>weights, </Strong> shown as
+              numbers alonging on edges in the tree display, indicate the
+              relative importance among the quality aspects in consideration.
+              Since the industry requirements could be various, users are
+              welcome to tune the weights by adjusting the corresponding
+              importance to prioritize different characteristics.
+            </Text>
+          </HoverCard.Content>
+        </HoverCard.Root>
+      </Box>
 
+      {/* callout card information */}
+      <Box>
+        <Callout.Root>
+          <Callout.Icon>
+            <InfoCircledIcon />
+          </Callout.Icon>
+          <Callout.Text>
+            Currently, the adjsutment is only applicable for{" "}
+            <Strong>Quality Characteristics</Strong>
+            (i.e., the 2nd level of the tree display, and the 2nd expandable box
+            in the list display).
+          </Callout.Text>
+        </Callout.Root>
+      </Box>
 
-    return (
-        <Flex direction="column" gap="3" align="start">
-            {/* title, right now we will keep it as "weight" */}
-            <Box>
-                <HoverCard.Root>
-                    <HoverCard.Trigger>
-                        <Link href="#" size='3' style={{ margin: '0px', }} > <GearIcon />  Dynamic Importance Adjustment</Link>
-                    </HoverCard.Trigger>
-                    <HoverCard.Content>
-                        <Text as="div" size="1" style={{ maxWidth: 325 }}>
+      <Box position={"relative"} left={"50%"} top={"50%"}>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <Button size="2" className="Button violet">
+              {" "}
+              Adjust{" "}
+            </Button>
+          </Dialog.Trigger>
 
-                            For Quality Chracteristics, <Strong>weights, </Strong> shown as numbers alonging on edges in the tree display,
-                            indicate the relative importance among the quality aspects in consideration.
-                            Since the industry requirements could be various,
-                            users are welcome to tune the weights by adjusting the corresponding importance to prioritize different characteristics.
+          <Dialog.Portal>
+            <Dialog.Overlay className="DialogOverlay" />
+            <Dialog.Content className="DialogContent">
+              <Dialog.Title className="DialogTitle">
+                Characteristics and corresponding weights
+              </Dialog.Title>
+              <Dialog.Description className="DialogDescription">
+                {/* Dialog description content */}
+                Adjust the importance of chrachteristics, the weights will be
+                recalculated automaticly.
+              </Dialog.Description>
 
-                        </Text>
-                    </HoverCard.Content>
-                </HoverCard.Root>
+              <Separator my="3" size="4" />
 
-            </Box>
+              {/*Profile selection*/}
+              <ProfileSelection onProfileChange={handleProfileApply} />
 
-            {/* callout card information */}
-            <Box>
-                <Callout.Root>
-                    <Callout.Icon>
-                        <InfoCircledIcon />
-                    </Callout.Icon>
-                    <Callout.Text>
-                        Currently, the adjsutment is only applicable for <Strong>Quality Characteristics</Strong>
-                        (i.e., the 2nd level of the tree display, and the 2nd expandable box in the list display).
-                    </Callout.Text>
-                </Callout.Root>
+              <Separator my="3" size="4" />
 
-            </Box>
+              {/* show the adjustment table*/}
+              <AdjustmentTable
+                selectedProfile={
+                  Array.isArray(selectedProfile) ? selectedProfile : undefined
+                }
+                isProfileApplied={isProfileApplied}
+                onResetApplied={handleReset}
+              />
 
-            <Box position={'relative'} left={'50%'} top={'50%'}>
-                <Dialog.Root>
-                    <Dialog.Trigger asChild>
-                        <Button size="2" className="Button violet"> Adjust </Button>
-                    </Dialog.Trigger>
-
-                    <Dialog.Portal>
-                        <Dialog.Overlay className="DialogOverlay" />
-                        <Dialog.Content className="DialogContent">
-                            <Dialog.Title className="DialogTitle">Characteristics and corresponding weights</Dialog.Title>
-                            <Dialog.Description className="DialogDescription">
-                                {/* Dialog description content */}
-                                Adjust the importance of chrachteristics, the weights will be recalculated automaticly.
-                            </Dialog.Description>
-
-                            <Separator my="3" size="4" />
-
-                            {/*Profile selection*/}
-                            <ProfileSelection onProfileChange={handleProfileApply} />
-
-                            <Separator my="3" size="4" />
-
-
-                            {/* show the adjustment table*/}
-                            <AdjustmentTable
-                                selectedProfile={Array.isArray(selectedProfile) ? selectedProfile : undefined}
-                                isProfileApplied={isProfileApplied}
-                                onResetApplied={handleReset} // Passing the reset handler down
-                            />
-
-
-                            {/* Position the close button absolutely within the Dialog.Content */}
-                            <Dialog.Close asChild>
-                                <IconButton className="IconButton" aria-label="Close">
-                                    <Cross2Icon />
-                                </IconButton>
-                            </Dialog.Close>
-
-                        </Dialog.Content>
-
-                    </Dialog.Portal>
-                </Dialog.Root>
-
-            </Box>
-
-        </Flex>
-
-
-    );
+              {/* Position the close button absolutely within the Dialog.Content */}
+              <Dialog.Close asChild>
+                <IconButton className="IconButton" aria-label="Close">
+                  <Cross2Icon />
+                </IconButton>
+              </Dialog.Close>
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
+      </Box>
+    </Flex>
+  );
 };
--- src/composites/ConfigurationContainer/ImportanceAdjustment/ProfileSelection.tsx
+++ src/composites/ConfigurationContainer/ImportanceAdjustment/ProfileSelection.tsx
@@ -1,92 +1,92 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Box, Select, Button, Flex, Text, Separator } from "@radix-ui/themes";
-import { ChevronDownIcon, UploadIcon, SliderIcon, DropdownMenuIcon } from '@radix-ui/react-icons';
-import { Profile } from '../../../types';
+import {
+  ChevronDownIcon,
+  UploadIcon,
+  SliderIcon,
+  DropdownMenuIcon,
+} from "@radix-ui/react-icons";
+import { Profile } from "../../../types";
 import profilesData from "../../../assets/PIQUE_json_files/ExampleProfile.json";
 
 interface ProfileSelectionProps {
-    onProfileChange: (selectedProfiles: Profile[] | null) => void;
+  onProfileChange: (selectedProfiles: Profile[] | null) => void;
 }
 
-export const ProfileSelection: React.FC<ProfileSelectionProps> = ({ onProfileChange }) => {
-    // Use state to manage the dynamically uploaded or customized profiles
-    const [selectedProfileType, setSelectedProfileType] = useState<string | null>(null);
-    const [uploadedProfiles, setUploadedProfiles] = useState<Profile[]>([]);
-
-    // Function to handle profile selection changes
-    const handleProfileChange = (value: string) => {
-        setSelectedProfileType(value);
-        const filteredProfiles = [...profilesData, ...uploadedProfiles].filter(profile => profile.type === value);
-        onProfileChange(filteredProfiles.length > 0 ? filteredProfiles : null);
-    };
-
-    // Function to handle file uploads
-    const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files?.[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                try {
-                    const uploadedProfiles: Profile[] = JSON.parse(e.target?.result as string);
-                    setUploadedProfiles(uploadedProfiles);
-                    onProfileChange(uploadedProfiles);
-                } catch (error) {
-                    console.error("Error parsing the uploaded file", error);
-                    onProfileChange(null);
-                }
-            };
-            reader.readAsText(file);
-        }
-    };
-
-
-    return (
-        <Flex direction={'column'}>
-            <Flex><Text size={'8'}> (Optional) Profile Selection</Text></Flex>
-            <Flex align={'center'} justify={'between'} gap={'6'} direction={'row'}>
-                <Separator my="3" size="3" />
-                {/* Select a predefined profile */}
-                <Flex direction={'column'} align={'center'} justify={'start'}>
-                    <Box>
-                        <Text> <DropdownMenuIcon /> Option 1: Predefined Industry Profiles: </Text>
-                    </Box>
-                    <Box>
-                        <Select.Root onValueChange={handleProfileChange} size={'3'} >
-                            <Select.Trigger style={{ width: "100px" }} aria-label="Profile" className="SelectTrigger" placeholder="None"> <ChevronDownIcon />
-                            </Select.Trigger>
-                            <Select.Content className="SelectContent" >
-                                {[...new Set([...profilesData, ...uploadedProfiles].map(profile => profile.type))].map((type, index) => (
-                                    <Select.Item className="SelectItem" key={index} value={type}>
-                                        <Select.Item value={type}>{type}</Select.Item>
-                                    </Select.Item>
-                                ))}
-                            </Select.Content>
-                        </Select.Root>
-                    </Box>
-                </Flex>
-
-                <Separator orientation="vertical" size={'3'} />
-
-                {/* Upload a self-defined profile */}
-                <Flex direction={'column'} align={'center'} justify={'start'}>
-                    <Box>  <UploadIcon /> <Text>Option 2: Personal profile</Text></Box>
-                    <Box> <input type="file" onChange={handleFileUpload} /> </Box>
-                </Flex>
-
-                <Flex>
-                    <Flex>
-                        <Button onClick={() => {
-                            // Find profiles that match the selected type
-                            const filteredProfiles = [...profilesData, ...uploadedProfiles].filter(profile => profile.type === selectedProfileType);
-                            // Trigger the parent component's change handler with the found profiles
-                            onProfileChange(filteredProfiles.length > 0 ? filteredProfiles : null);
-                        }}>Apply</Button>
-                    </Flex>
-                </Flex>
-
-
-
-            </Flex>
-        </Flex>
+export const ProfileSelection: React.FC<ProfileSelectionProps> = ({
+  onProfileChange,
+}) => {
+  // Function to handle profile selection changes
+  const handleProfileChange = (value: string) => {
+    const filteredProfiles = profilesData.filter(
+      (profile) => profile.type === value
     );
+    onProfileChange(filteredProfiles.length > 0 ? filteredProfiles : null);
+  };
+
+  // Function to handle file uploads
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        try {
+          const uploadedProfiles: Profile[] = JSON.parse(
+            e.target?.result as string
+          );
+          onProfileChange(uploadedProfiles);
+        } catch (error) {
+          console.error("Error parsing the uploaded file", error);
+          onProfileChange(null);
+        }
+      };
+      reader.readAsText(file);
+    }
+  };
+
+  return (
+    <Flex direction={"column"}>
+      <Flex>
+        <Text size={"8"}> (Optional) Profile Selection</Text>
+      </Flex>
+      <Flex align={"center"} justify={"between"} gap={"6"} direction={"row"}>
+        <Separator my="3" size="3" />
+        <Flex direction={"column"} align={"center"} justify={"start"}>
+          <Box>
+            <Text>
+              {" "}
+              <DropdownMenuIcon /> Option 1: Predefined Industry Profiles:{" "}
+            </Text>
+          </Box>
+          <Box>
+            <Select.Root onValueChange={handleProfileChange} defaultValue="">
+              <Select.Trigger aria-label="Profile" className="trigger">
+                <ChevronDownIcon />
+              </Select.Trigger>
+              <Select.Content className="content">
+                {profilesData.map((profile, index) => (
+                  <Select.Item key={index} value={profile.type}>
+                    {profile.type}
+                  </Select.Item>
+                ))}
+              </Select.Content>
+            </Select.Root>
+          </Box>
+        </Flex>
+
+        <Separator orientation="vertical" size={"3"} />
+
+        <Flex direction={"column"} align={"center"} justify={"start"}>
+          <Box>
+            {" "}
+            <UploadIcon /> <Text>Option 2: Personal profile</Text>
+          </Box>
+          <Box>
+            {" "}
+            <input type="file" onChange={handleFileUpload} />{" "}
+          </Box>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 };
